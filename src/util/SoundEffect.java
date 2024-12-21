e25c062304d3f84734367f85ad9bf27ef92e3f6b
yuzuzu2266 <12310543@mail.sustech.edu.cn>
2024-12-21 19:01:01 +0800

    加了音乐

---
+++ src/util/SoundEffect.java
@@ -0,0 +1,39 @@
+package util;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+public class SoundEffect {
+    private Clip clip;
+
+    public SoundEffect(String filePath) {
+        try {
+            // 加载音频文件
+            File soundFile = new File(filePath);
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+
+            // 获取音频格式
+            AudioFormat format = audioInputStream.getFormat();
+
+            // 创建数据行信息
+            DataLine.Info info = new DataLine.Info(Clip.class, format);
+
+            // 获取音频剪辑
+            clip = (Clip) AudioSystem.getLine(info);
+
+            // 打开音频剪辑并加载音频数据
+            clip.open(audioInputStream);
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void play() {
+        if (clip != null) {
+            clip.setFramePosition(0); // 重置音频到开头
+            clip.start(); // 开始播放
+        }
+    }
+
+}
