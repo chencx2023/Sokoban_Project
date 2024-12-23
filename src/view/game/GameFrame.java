08259f9cc183f84130e1ea68ec7e8982d3b274d0
yuzuzu2266 <12310543@mail.sustech.edu.cn>
2024-12-23 14:37:59 +0800

    添加计时器

--- src/view/game/GameFrame.java
+++ src/view/game/GameFrame.java
@@ -15,6 +15,9 @@ import model.MapMatrix;
 import view.FrameUtil;
 import view.level.LevelFrame;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class GameFrame extends JFrame {
 
     private GameController controller;
@@ -34,6 +37,11 @@ public class GameFrame extends JFrame {
     private JLabel trailLabel;
     private GamePanel gamePanel;
     private Clip clickSound;
+
+    private Timer timer;
+    private int seconds=0;
+    private JLabel timeLabel;
+
 
     public GameFrame(int width, int height, MapMatrix mapMatrix, FrameController frameController) {
         this.frameController = frameController;
@@ -77,6 +85,16 @@ public class GameFrame extends JFrame {
 
         gamePanel.setStepLabel(stepLabel);
         gamePanel.setTrailLabel(trailLabel);
+
+        this.timeLabel=FrameUtil.createJLabel(this, "Time:0", new Font("serif", Font.ITALIC, 22), new Point(gamePanel.getWidth() + 80, 600), 180, 50);
+        timer=new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                seconds++;
+                updateTimeLabel();
+            }
+        });
+        timer.start();
 
         addButtonListeners();
 
@@ -220,6 +238,16 @@ public class GameFrame extends JFrame {
         }
         super.dispose();
     }
+
+    //更新计时器标签
+    private void updateTimeLabel(){
+        timeLabel.setText("Time:"+seconds);
+    }
+    //停止计时器
+    public void stopTimer(){
+        timer.stop();
+    }
+
 
     public FrameController getFrameController() {
         return frameController;
